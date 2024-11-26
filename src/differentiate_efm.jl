967324f5b5a492240bd982f3ddb2e614c5715e0d
HettieC <hester.chapman@btinternet.com>
2024-11-26 16:35:29 +0100

    type of D

--- src/differentiate_efm.jl
+++ src/differentiate_efm.jl
@@ -128,7 +128,7 @@ function cost_matrix(
     evaluate=false,
     parameter_values=nothing
 )
-    D = Matrix{Real}(undef, length(capacity), length(EFMs))
+    D = Matrix{Float64}(undef, length(capacity), length(EFMs))
     for (i, (enzyme_group, enzymes, enzyme_bound)) in enumerate(capacity)
         for (j, efm) in enumerate(EFMs)
             D[i, j] = 0
