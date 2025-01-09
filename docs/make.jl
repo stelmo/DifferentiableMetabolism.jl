a10e1f11707e426ce2161e365ae861170b78199e
St. Elmo Wilken <stelmozors@gmail.com>
2025-01-09 11:26:09 +0100

    docs again

--- docs/make.jl
+++ docs/make.jl
@@ -43,7 +43,7 @@ makedocs(
         "README" => "index.md",
         "Examples" => [
             "Contents" => "examples.md"
-            examples_mds
+            example_mds
         ],
         "Reference" => "reference.md",
     ],
