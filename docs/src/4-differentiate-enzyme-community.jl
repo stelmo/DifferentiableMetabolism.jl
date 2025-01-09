df2e30804ec31d1dae21ab6a42812f7efe051257
St. Elmo Wilken <stelmozors@gmail.com>
2025-01-09 18:04:36 +0100

    implement community model

--- docs/src/4-differentiate-enzyme-community.jl
+++ docs/src/4-differentiate-enzyme-community.jl
@@ -15,6 +15,8 @@
 # limitations under the License.                                           #src
 
 # # Differentiating enzyme constrained metabolic models
+# Construct a community of two interacting E. coli's that share oxoglutarate and
+# glutamine.
 
 import DifferentiableMetabolism as D
 import FastDifferentiation as F
@@ -26,9 +28,6 @@ import COBREXA as X
 import Tulip as T
 import Downloads: download
 import CairoMakie as CM
-
-!isfile("e_coli_core.json") &&
-    download("http://bigg.ucsd.edu/static/models/e_coli_core.json", "e_coli_core.json")
 
 include("../../test/data_static.jl")
 
@@ -43,6 +42,7 @@ end
 model.reactions["EX_glc__D_e"].lower_bound = -1000.0 # capacity bound suffices
 model.reactions["PFL"].upper_bound = 0.0 # aerobic simulation
 
+# add a transporter for glutamine
 model.reactions["GLNt"] = A.CanonicalModel.Reaction(;
     name = "Glutamine transporter",
     lower_bound = -1000.0,
@@ -52,11 +52,10 @@ model.reactions["GLNt"] = A.CanonicalModel.Reaction(;
         "gln__L_c" => 1.0,
         "h_e" => -1.0,
         "h_c" => 1.0,
-    )
+    ),
 )
 
-
-reaction_isozymes = Dict{String,Dict{String, X.Isozyme}}() 
+reaction_isozymes = Dict{String,Dict{String,X.Isozyme}}()
 
 # Populate reaction_isozymes with parameters
 for rid in A.reactions(model)
@@ -68,80 +67,78 @@ for rid in A.reactions(model)
 
         kcat = ecoli_core_reaction_kcats[rid] * 3.6 # change unit to k/h
 
-        d = get!(reaction_isozymes, rid, Dict{String, X.Isozyme}())
+        d = get!(reaction_isozymes, rid, Dict{String,X.Isozyme}())
         d["isozyme_$i"] = X.Isozyme(
             gene_product_stoichiometry = Dict(grr .=> fill(1.0, size(grr))),
-            kcat_forward = kcat, 
-            kcat_reverse = kcat, 
-        ) 
+            kcat_forward = kcat,
+            kcat_reverse = kcat,
+        )
     end
 end
 
 # Add gene product molar mass and capacity constraint info
 gene_product_molar_masses = Dict(k => v for (k, v) in ecoli_core_gene_product_masses)
 
-wt = X.enzyme_constrained_flux_balance_constraints( # reference model
+wt = X.enzyme_constrained_flux_balance_constraints( # reference model, will be used to get some bound info from
     model;
     reaction_isozymes,
     gene_product_molar_masses,
     capacity = 50.0,
-    interface = :identifier_prefixes
+    interface = :identifier_prefixes,
 )
 
 km = X.optimized_values( #src
-    wt,
+    wt, #src
     optimizer = T.Optimizer, #src
-    objective = wt.objective.value,
+    objective = wt.objective.value, #src
     settings = [X.set_optimizer_attribute("IPM_IterationsLimit", 10_000)], #src
 ) #src
-@test isapprox(km.objective, 0.8865945023142839, atol=TEST_TOLERANCE) #src
+@test isapprox(km.objective, 0.8865945023142839, atol = TEST_TOLERANCE) #src
 
-glutamine_ko = deepcopy(model) # cannot produce glutamine
-delete!(glutamine_ko.reactions, "GLNS") # KO reaction
+gln_ko = deepcopy(model) # cannot produce glutamine
+delete!(gln_ko.reactions, "GLNS") # KO reaction
 
 km = X.enzyme_constrained_flux_balance_analysis( #src
-    glutamine_ko; #src
+    gln_ko; #src
     reaction_isozymes, #src
     gene_product_molar_masses, #src
     capacity = 50.0, #src
     optimizer = T.Optimizer, #src
     settings = [X.set_optimizer_attribute("IPM_IterationsLimit", 10_000)], #src
 ) #src
-@test isapprox(km.objective, 0.0, atol=TEST_TOLERANCE) #src
+@test isapprox(km.objective, 0.0, atol = TEST_TOLERANCE) #src
 
-glutamine_ko.reactions["EX_gln__L_e"].lower_bound = -1000.0 # open exchange bound to other organism
+gln_ko.reactions["EX_gln__L_e"].lower_bound = -1000.0 # open exchange bound to other organism
 
-
-oxoglutarate_ko = deepcopy(model) # cannot produce oxoglutarate
-delete!(oxoglutarate_ko.reactions, "ICDHyr") # KO reaction
+akg_ko = deepcopy(model) # cannot produce oxoglutarate
+delete!(akg_ko.reactions, "ICDHyr") # KO reaction
 
 km = X.enzyme_constrained_flux_balance_analysis( #src
-    oxoglutarate_ko; #src
+    akg_ko; #src
     reaction_isozymes, #src
     gene_product_molar_masses, #src
     capacity = 50.0, #src
     optimizer = T.Optimizer, #src
     settings = [X.set_optimizer_attribute("IPM_IterationsLimit", 10_000)], #src
 ) #src
-@test isapprox(km.objective, 0.0, atol=TEST_TOLERANCE) #src
+@test isapprox(km.objective, 0.0, atol = TEST_TOLERANCE) #src
 
-oxoglutarate_ko.reactions["EX_akg_e"].lower_bound = -1000.0 # open exchange bound to other organism
+akg_ko.reactions["EX_akg_e"].lower_bound = -1000.0 # open exchange bound to other organism
 
 ec_gln_ko = X.enzyme_constrained_flux_balance_constraints(
-    glutamine_ko;
+    gln_ko;
     reaction_isozymes,
     gene_product_molar_masses,
     capacity = 50.0,
-    interface = :identifier_prefixes
+    interface = :identifier_prefixes,
 )
 
-
 ec_akg_ko = X.enzyme_constrained_flux_balance_constraints(
-    oxoglutarate_ko;
+    akg_ko;
     reaction_isozymes,
     gene_product_molar_masses,
     capacity = 50.0,
-    interface = :identifier_prefixes
+    interface = :identifier_prefixes,
 )
 
 # bound the exchanges - adopt similar bounds to wt wrt to the environment
@@ -152,7 +149,7 @@ end
 
 # create KO pairs to be joined via their exchanges
 ko_pairs = [
-    :gln => (ec_gln_ko, ec_gln_ko.interface.exchanges, 0.5)
+    :gln => (ec_gln_ko, ec_gln_ko.interface.exchanges, 0.5) # simulate at abundance of 0.5 for each organism
     :akg => (ec_akg_ko, ec_akg_ko.interface.exchanges, 0.5)
 ]
 
@@ -160,23 +157,156 @@ ko_pairs = [
 x = X.interface_constraints(ko_pairs...; bound = boundf)
 
 # set all growth rates equal
-x *= :equalgrowth^C.Constraint(
-    x.gln.objective.value - x.akg.objective.value,
-    C.EqualTo(0)
-)
+x *= :equalgrowth^C.Constraint(x.gln.objective.value - x.akg.objective.value, C.EqualTo(0))
 
 # set objective as any of the biomass functions (they are constrained equal)
-x *= :objective^C.Constraint(
-    x.gln.objective.value,
-    nothing,
-)
+x *= :objective^C.Constraint(x.gln.objective.value, nothing)
 
-sol = X.optimized_values(
+sol = X.optimized_values( # test that community can grow
     x;
     optimizer = T.Optimizer,
     objective = x.objective.value,
     sense = X.Maximal,
-    settings = [X.set_optimizer_attribute("IPM_IterationsLimit", 10_000)], #src
+    settings = [X.set_optimizer_attribute("IPM_IterationsLimit", 10_000)],
+)
+
+@test isapprox(sol.objective, 0.8641022797127501, atol = TEST_TOLERANCE) #src
+
+# now prune the base models
+# slighty more involved this time
+
+#md # !!! info "Pruning the model can be tricky"
+#md #     Important to ensure no zero fluxes are included in the model - uniqueness.
+
+p_akg_ko, p_r_iso_akg = D.prune_model(
+    akg_ko,
+    sol.akg.fluxes,
+    sol.akg.gene_product_amounts,
+    reaction_isozymes,
+    sol.akg.isozyme_forward_amounts,
+    sol.akg.isozyme_reverse_amounts,
+    1e-6,
+    1e-6,
+);
+
+p_gln_ko, p_r_iso_gln = D.prune_model(
+    gln_ko,
+    sol.gln.fluxes,
+    sol.gln.gene_product_amounts,
+    reaction_isozymes,
+    sol.gln.isozyme_forward_amounts,
+    sol.gln.isozyme_reverse_amounts,
+    1e-6,
+    1e-6,
+);
+
+ec_gln_ko = X.enzyme_constrained_flux_balance_constraints(
+    p_gln_ko;
+    reaction_isozymes = p_r_iso_gln,
+    gene_product_molar_masses,
+    capacity = 50.0,
+    interface = :identifier_prefixes,
+)
+
+ec_akg_ko = X.enzyme_constrained_flux_balance_constraints(
+    p_akg_ko;
+    reaction_isozymes = p_r_iso_akg,
+    gene_product_molar_masses,
+    capacity = 50.0,
+    interface = :identifier_prefixes,
 )
 
 
+# bound the exchanges - adopt similar bounds to wt wrt to the environment
+env = deepcopy(ec_gln_ko.interface.exchanges)
+
+# delete the exchange of akg and gln to the environment! Will introduce a 0 flux if not
+delete!(env, :EX_akg_e)
+delete!(env, :EX_gln__L_e)
+env
+
+boundf(id) = begin
+    ex_id = first(id)
+    env[ex_id].bound
+end
+
+# ignore the deleted bound, it is still in the interface of each organism and will generate an error if not ignored
+ignoref(id, p) = begin
+    (id == :gln || id == :akg) && (:EX_gln__L_e in p || :EX_akg_e in p)
+end
+
+# investigate the sensitivity of the variables wrt to the abundances of each organism
+F.@variables a_gln a_akg
+
+# add parameters into community construction
+ko_pairs = [
+    :gln => (ec_gln_ko, ec_gln_ko.interface.exchanges, a_gln)
+    :akg => (ec_akg_ko, ec_akg_ko.interface.exchanges, a_akg)
+]
+
+# create community model (interface joins them)
+x = X.interface_constraints(ko_pairs...; bound = boundf, ignore = ignoref)
+
+# join partner exchanges - need to do this to avoid making a 0 env variable
+x.interface_balance *=
+    :akg^C.Constraint(
+        a_akg * x.akg.interface.exchanges.EX_akg_e.value -
+        a_gln * x.gln.interface.exchanges.EX_akg_e.value,
+        C.EqualTo(0),
+    )
+x.interface_balance *=
+    :gln^C.Constraint(
+        a_akg * x.akg.interface.exchanges.EX_gln__L_e.value -
+        a_gln * x.gln.interface.exchanges.EX_gln__L_e.value,
+        C.EqualTo(0),
+    )
+
+# set all growth rates equal
+x *= :equalgrowth^C.Constraint(x.gln.objective.value - x.akg.objective.value, C.EqualTo(0))
+
+# set objective as any of the biomass functions (they are constrained equal)
+x *= :objective^C.Constraint(x.gln.objective.value, nothing)
+
+param_vals = Dict(:a_gln => 0.5, :a_akg => 0.5)
+
+psol = D.optimized_values(
+    x,
+    param_vals;
+    optimizer = T.Optimizer,
+    objective = x.objective.value,
+    sense = X.Maximal,
+    settings = [X.set_optimizer_attribute("IPM_IterationsLimit", 10_000)],
+)
+psol.tree
+
+@test isapprox(sol.objective, psol.tree.objective, atol = TEST_TOLERANCE) #src
+
+# differentiate wrt to the params
+dparams = [:a_akg, :a_gln]
+
+# prepare derivatives
+pm_kkt, vids = D.differentiate_prepare_kkt(x, x.objective.value, dparams)
+
+sens = D.differentiate_solution(
+    pm_kkt,
+    psol.primal_values,
+    psol.equality_dual_values,
+    psol.inequality_dual_values,
+    param_vals,
+    scale = true, # unitless sensitivities
+)
+
+# only look at how the abundances impact the environmental exchanges
+env_exs = string.(last.(vids)[end-7:end])
+
+# lets look at the environmental exchanges
+fig, ax, hm = CM.heatmap(
+    sens[end-7:end, :];
+    axis = (
+        xticks = (1:8, env_exs),
+        xticklabelrotation = -pi / 2,
+        yticks = (1:2, string.(dparams)),
+    ),
+)
+CM.Colorbar(fig[1, 2], hm)
+fig
