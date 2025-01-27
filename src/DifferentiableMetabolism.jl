b919ceff82ee6dd7ad7a855e8fb1a0f0945a6b81
St. Elmo Wilken <stelmozors@gmail.com>
2025-01-27 17:50:09 +0100

    add a little more module info

--- src/DifferentiableMetabolism.jl
+++ src/DifferentiableMetabolism.jl
@@ -16,6 +16,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 =#
 
+"""
+    module DifferentiableMetabolism
+
+Differentiate parameterized genome-scale metabolic models. Currently,
+`FastDifferentiation` parameters are used in conjunction with `COBREXA` models.
+To ensure your derivatives are well defined, you must ensure that the models are
+*pruned*, i.e. all active reactions, genes, etc. have nonzero values at the
+optimum. See the documentation for model details and examples.
+"""
 module DifferentiableMetabolism
 
 using DocStringExtensions
